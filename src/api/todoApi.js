c481342d4bae23f08735fe5751b3985a5be24160
ShinDongJae <sdhsdj6450@gmail.com>
2023-06-17 16:56:30 +0900

    error: access token

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -1,32 +1,38 @@
 import { getLocalStorage } from "../helper/localStorageHandler";
 import Axios from "./const";
 
-const KEY = getLocalStorage("token");
-
 export const createTodo_API = (todo) => {
+  const { access_token } = getLocalStorage("token");
+
   const url = "/todos";
   return Axios.post(url, todo, {
-    headers: { Authorization: `Bearer ${KEY?.access_token}` },
+    headers: { Authorization: `Bearer ${access_token}` },
   });
 };
 
 export const getTodo_API = () => {
+  const { access_token } = getLocalStorage("token");
+
   const url = "/todos";
   return Axios.get(url, {
-    headers: { Authorization: `Bearer ${KEY?.access_token}` },
+    headers: { Authorization: `Bearer ${access_token}` },
   });
 };
 
 export const updateTodo_API = (id, body) => {
+  const { access_token } = getLocalStorage("token");
+
   const url = `/todos/${id}`;
   return Axios.put(url, body, {
-    headers: { Authorization: `Bearer ${KEY?.access_token}` },
+    headers: { Authorization: `Bearer ${access_token}` },
   });
 };
 
 export const deleteTodo_API = (id) => {
+  const { access_token } = getLocalStorage("token");
+
   const url = `/todos/${id}`;
   return Axios.delete(url, {
-    headers: { Authorization: `Bearer ${KEY?.access_token}` },
+    headers: { Authorization: `Bearer ${access_token}` },
   });
 };
