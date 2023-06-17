d8cb3de87222df2f385049c896def7b187d1c381
ShinDongJae <sdhsdj6450@gmail.com>
2023-06-17 16:38:24 +0900

    feat: home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Link to="/signin">Login</Link>
       <Link to="/signup">Signup</Link>
       <Link to="/todo">Todo</Link>
